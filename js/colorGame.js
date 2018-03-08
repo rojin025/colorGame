5b5f9ffcbff2d80cf49f74b09e12726abcf1368f
Rojin BIjukchhe <35796647+rojin025@users.noreply.github.com>
2018-03-07 23:07:59 -0800

    v.1.3

--- js/colorGame.js
+++ js/colorGame.js
@@ -6,3 +6,10 @@ var colors = [
   "rgb(0, 255, 255)",
   "rgb(0, 0, 255)"
 ]
+var squares = document.querySelectorAll("#square");
+var selectedWinColor = colors[2];
+
+ for (var i = 0; i < squares.Length; i++){
+   squares[i].style.backgroundColor = colors[i];
+
+ };
