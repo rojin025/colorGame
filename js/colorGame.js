1a105df1d04cf90f5ef9f83438b4be002cf856c8
Rojin BIjukchhe <35796647+rojin025@users.noreply.github.com>
2018-03-08 11:50:25 -0800

    v.1.6`
    added some clicked

--- js/colorGame.js
+++ js/colorGame.js
@@ -8,13 +8,24 @@ var colors = [
 ]
 var squares = document.querySelectorAll(".square");
 
-var selectedWinColor = colors[2];
-var colorDisplay = document.getElementById("colorDisplay");
+var colorDisplay = document.getElementById("colorDisplay");//pulled top RGB section from HTML
+var selectedWinColor = colors[2];//winning color
 
 colorDisplay.textContent = selectedWinColor;
 for (var i = 0; i < squares.length; i++){
+  //added colors to squares
   squares[i].style.backgroundColor = colors[i];
-  if
+  //adding event listener to select different selected squares
+  squares[i].addEventListener("click", function() {
+    //grab color from picked colors
+    var clickedColor = this.style.backgroundColor;
+    //compare color with winnig colors
+    if(clickedColor === selectedWinColor){
+      alert("Win");
+    } else {
+      alert("Lose");      
+    }
+  });
 };
 
 
