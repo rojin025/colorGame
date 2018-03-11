261b2c7a7da4c8be90319e2cc1549263540c6d50
Rojin BIjukchhe <35796647+rojin025@users.noreply.github.com>
2018-03-10 23:50:29 -0800

    3.3.4

--- js/colorGame.js
+++ js/colorGame.js
@@ -160,13 +160,13 @@ for(var i = 0; i < squares.length; i++){
 // 	//return that array
 // 	return arr;
 // }
-
-function randomColor() {
-	//pick a "red" from 0 - 255
-	var r = Math.floor(Math.random() * 256);
-	//pick a "green" from 0 - 255
-	var g = Math.floor(Math.random() * 256);
-	//pick a "blue" from 0 - 255
-	var b = Math.floor(Math.random() * 256);
-	return "rgb(" + r + ", " + g + ", " + b + ")";
-}
+// 
+// function randomColor() {
+// 	//pick a "red" from 0 - 255
+// 	var r = Math.floor(Math.random() * 256);
+// 	//pick a "green" from 0 - 255
+// 	var g = Math.floor(Math.random() * 256);
+// 	//pick a "blue" from 0 - 255
+// 	var b = Math.floor(Math.random() * 256);
+// 	return "rgb(" + r + ", " + g + ", " + b + ")";
+// }
