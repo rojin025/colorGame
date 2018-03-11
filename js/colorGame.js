ea3504ce3772e19b9067b24b69fa179161be595f
Rojin BIjukchhe <35796647+rojin025@users.noreply.github.com>
2018-03-10 23:46:54 -0800

    3.2

--- js/colorGame.js
+++ js/colorGame.js
@@ -68,6 +68,23 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+
+easyBtn.addEventListener("click", function() {
+	easyBtn.classList.add("selected");
+	hardBtn.classList.remove("selected");
+	numSquares = 3;
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for(var i = 0; i < squares.length; i++) {
+		if(colors[i]) {
+			squares[i].style.background = colors[i];
+		} else {
+			squares[i].style.display = "none";
+		}
+	}
+});
 
 resetButton.addEventListener("click", function(){
 	//generate all new colorDisplay
